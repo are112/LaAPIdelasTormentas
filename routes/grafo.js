c5bf3d9cbab84fb9480a37fe79e7533f1290a37c
Alexis <62334048+are112@users.noreply.github.com>
2026-05-14 12:18:53 +0200

    Add files via upload

--- routes/grafo.js
+++ routes/grafo.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { grafoCompleto, grafoPersonaje, grafoStats } from "../controllers/grafoController.js";
+import { grafoCompleto, grafoEntidad, grafoStats } from "../controllers/grafoController.js";
 
 const router = express.Router();
 
-// Orden importante: rutas estáticas antes que dinámicas
 router.get("/stats", grafoStats);
-router.get("/:id",   grafoPersonaje);
+router.get("/:id",   grafoEntidad);
 router.get("/",      grafoCompleto);
 
 export default router;
